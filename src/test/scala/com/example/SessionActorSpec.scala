fe7e68e2b7dffb442567a7bff0ed46e8570544da
sagun <sagun.b@gmail.com>
2016-12-05 15:01:39 -0500

    update the test

---
+++ src/test/scala/com/example/SessionActorSpec.scala
@@ -0,0 +1,35 @@
+package com.example
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestActor, TestActorRef, TestKit, TestProbe}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+class SessionActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("MySpec"))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A Session actor" must {
+    "store session history" in {
+      val sessionActor = TestActorRef[SessionActor](SessionActor.props)
+
+      sessionActor ! EventReader.EventMessage(1L, 1L, "/", "google", "firefox")
+      sessionActor ! EventReader.EventMessage(1L, 2L, "/", "google", "firefox")
+      sessionActor ! EventReader.Tick(2L)
+
+      assert(sessionActor.underlyingActor.history ==
+        collection.mutable.ListBuffer(
+          EventReader.EventMessage(1L, 2L, "/", "google", "firefox"),
+          EventReader.EventMessage(1L, 1L, "/", "google", "firefox")
+        )
+      )
+
+    }
+  }
+
+
+}
