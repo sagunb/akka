9440cf3981140e299cfa9367dd82504d5023ea59
sagun <sagun.b@gmail.com>
2016-12-05 15:53:51 -0500

    update applicationmain

--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -4,15 +4,11 @@ import akka.actor.ActorSystem
 
 object ApplicationMain extends App {
   val system = ActorSystem("MyActorSystem")
-  //  val pingActor = system.actorOf(PingActor.props, "pingActor")
+
   val statsActor = system.actorOf(StatsActor.props, "statsActor")
   val requestProxy = system.actorOf(RequestProxy.props(statsActor), "requestProxyActor")
   val eventReader = system.actorOf(EventReader.props("/Users/sagun/Downloads/exercises/events-1k.txt", requestProxy), "eventReaderActor")
 
   eventReader ! EventReader.Initialize
-
-//  pingActor ! PingActor.Initialize
-  // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
-  // see counter logic in PingActor
   system.awaitTermination()
 }
