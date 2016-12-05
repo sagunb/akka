484376564741eba806529a36b1c3f5722872f27f
sagun <sagun.b@gmail.com>
2016-12-05 14:06:48 -0500

    add test

--- src/test/scala/com/example/EventReaderSpec.scala
+++ src/test/scala/com/example/EventReaderSpec.scala
@@ -1,8 +1,34 @@
 package com.example
 
-/**
-  * Created by sagun on 2016-12-05.
-  */
-class EventReaderSpec {
+import akka.actor.ActorSystem
+import akka.actor.Actor
+import akka.actor.Props
+import akka.testkit.{ImplicitSender, TestActors, TestKit, TestProbe}
+import org.scalatest.WordSpecLike
+import org.scalatest.Matchers
+import org.scalatest.BeforeAndAfterAll
+
+class EventReaderSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("MySpec"))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "An Event Reader actor" must {
+    "send all messages in test file" in {
+      val testActor = TestProbe()
+      val eventActor = system.actorOf(EventReader.props("/Users/sagun/Downloads/exercises/test.txt", testActor.ref))
+      eventActor ! EventReader.Initialize
+      testActor.expectMsg(EventReader.Tick(1480534410848L))
+      testActor.expectMsg(EventReader.EventMessage(795253081L, 1480534410848L, "/", "google", "firefox"))
+      testActor.expectMsg(EventReader.EventMessage(795253081L, 1480534410858L, "/", "google", "firefox"))
+      testActor.expectMsg(EventReader.Tick(1480534410955L))
+      testActor.expectMsg(EventReader.EventMessage(795253081L, 1480534410955L, "/", "google", "firefox"))
+    }
+  }
+
 
 }
